b0426133e5775bccce2a6059c5a038ab0d4d080e
Achour Amine <47825029+AchAmine@users.noreply.github.com>
2022-10-09 10:54:26 +0100

    sign up & login

---
+++ app/src/main/java/com/esprit/myapp/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.esprit.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn ;
+    private TextView signupTv , forgotPwTv;
+    private Intent intent ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signupTv = findViewById(R.id.tvSignup);
+        signupTv.setOnClickListener(e -> {
+            intent = new Intent(LoginActivity.this , signupActivity.class);
+            startActivity(intent);
+        });
+
+
+    }
+}
