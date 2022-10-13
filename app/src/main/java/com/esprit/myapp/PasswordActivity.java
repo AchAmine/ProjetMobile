e8d089f6796aa7e8c29b22c0bb3008200d4c29fe
Achour Amine <47825029+AchAmine@users.noreply.github.com>
2022-10-13 13:11:24 +0100

    avant validation

--- app/src/main/java/com/esprit/myapp/PasswordActivity.java
+++ app/src/main/java/com/esprit/myapp/PasswordActivity.java
@@ -2,13 +2,22 @@ package com.esprit.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class PasswordActivity extends AppCompatActivity {
-
+    private Intent intent;
+    private ImageView profileBtn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
+
+        profileBtn = findViewById(R.id.goToProf);
+        profileBtn.setOnClickListener(e -> {
+            intent = new Intent(PasswordActivity.this , ProfilActivity.class);
+            startActivity(intent);
+        });
     }
 }
